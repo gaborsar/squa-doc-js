98545bdc868ad550a0eebd4f38b2974b2af5a7d7
Gabor Sar <gabor.sar@gmail.com>
2018-02-19 17:37:33 +0000

    Add react-polyfill to the example.

--- website/src/index.js
+++ website/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import initialContents from "./data/initialContents";
+import "babel-polyfill";
 import "./main.scss";
 
 ReactDOM.render(
