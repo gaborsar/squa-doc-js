869be1948ecd11e80493f8d72cd272b47f0fa5b2
Gabor Sar <gabor.sar@gmail.com>
2018-03-07 16:51:09 +0000

    Clean RangeBuilder.

--- packages/squa-editor/src/model/RangeBuilder.js
+++ packages/squa-editor/src/model/RangeBuilder.js
@@ -3,23 +3,23 @@ import Range from "./Range";
 
 export default class RangeBuilder {
   constructor(nodes) {
-    this._iterator = new RangeIterator(nodes);
-    this._elements = [];
+    this.iterator = new RangeIterator(nodes);
+    this.elements = [];
   }
 
   skip(length) {
-    this._iterator.next(length);
+    this.iterator.next(length);
     return this;
   }
 
   keep(length) {
-    this._iterator.next(length, element => {
-      this._elements.push(element);
+    this.iterator.next(length, element => {
+      this.elements.push(element);
     });
     return this;
   }
 
   build() {
-    return new Range(this._elements);
+    return new Range(this.elements);
   }
 }
