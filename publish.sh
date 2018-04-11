abf19a51c8cb9a3baaed9e4e22d1f87ad4479ba4
Gabor Sar <gabor.sar@gmail.com>
2018-04-11 10:49:10 +0100

    Improved default editing handlers

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd packages/squa-doc-js
+npm publish
+
+cd ../squa-doc-js-block-image-plugin
+npm publish
+
+cd ../squa-doc-js-inline-image-plugin
+npm publish
+
+cd ../squa-doc-js-checkable-plugin
+npm publish
+
+cd ../squa-doc-js-outline-plugin
+npm publish
